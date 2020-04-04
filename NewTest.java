be6e2eb66bed6d592ee9f4b3d7008ba67962cf85
bhanuchand1415 <com.bhanuchand.javatester@gmail.com>
2020-04-04 16:21:06 +0530

    pull code

---
+++ NewTest.java
@@ -0,0 +1,14 @@
+package com.bHaNuChand.TestNGTest;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+
+	
+  public void f() {
+	
+					
+	  System.out.println("PlzPractice");
+	  
+  }
+}
